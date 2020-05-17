31b1da0499fb12e15059616229e7fcf1f724c77e
Ivo Kubjas <ivo@teokon.eu>
2020-05-17 16:18:54 +0300

    use package-local state

--- build/build-qpkg.sh
+++ build/build-qpkg.sh
@@ -12,18 +12,21 @@ cp out/tailscale-amd64 /Tailscale/x86_64/tailscale
 cp out/tailscaled-arm64 /Tailscale/arm_64/tailscaled
 cp out/tailscale-arm64 /Tailscale/arm_64/tailscale
 
+mkdir -p /Tailscale/shared/var/run/tailscale
+mkdir -p /Tailscale/shared/var/lib/tailscale
+
 sed -i '/#QPKG_REQUIRE/cQPKG_REQUIRE="QVPN"' /Tailscale/qpkg.cfg 
 
 sed -i '/: ADD START ACTIONS HERE/c\
-    $QPKG_ROOT/tailscaled --port 41641 2> /dev/null &\
-    echo $! > /var/run/tailscale.pid\
+    $QPKG_ROOT/tailscaled --port 41641 --state=$QPKG_ROOT/var/lib/tailscale/tailscaled.state --socket=$QPKG_ROOT/var/run/tailscale/tailscaled.sock 2> /dev/null &\
+    echo $! > $QPKG_ROOT/var/run/tailscale/tailscale.pid\
     sleep 3' /Tailscale/shared/Tailscale.sh
 
 sed -i '/: ADD STOP ACTIONS HERE/c\
-    ID=$(more /var/run/tailscale.pid)\
-    if [ -e /var/run/tailscale.pid ]; then\
+    ID=$(more $QPKG_ROOT/var/run/tailscale/tailscale.pid)\
+    if [ -e $QPKG_ROOT/var/run/tailscale/tailscale.pid ]; then\
         kill -9 $ID\
-        rm -f /var/run/tailscale.pid\
+        rm -f $QPKG_ROOT/var/run/tailscale/tailscale.pid\
     fi' /Tailscale/shared/Tailscale.sh
 
 qbuild --root /Tailscale --build-arch x86 --build-dir /out/pkg
